f56a75deee0a5fdbc6dc248ea1a883409f9cbd24
EgoMoose <Ego.Moose@gmail.com>
2015-09-26 16:09:57 -0400

    Added template
    Added an empty template for new font creation.

---
+++ src/Lua/fontTemplate_Module.lua
@@ -0,0 +1,21 @@
+--[[
+	Font template
+	@ Font Name_here
+	@ Sizes Sizes_here
+	@ Author Username_here
+	@ Link link_here
+--]]
+
+-- Place atlas image assets in this table in the order they were generated
+local img = {
+	[1] = "rbxassetid://";
+}
+
+-- Paste the generated .fnt JSON inbetween these brackets
+local json = [[]];
+
+
+return {
+	image = img;
+	json = json;
+};
